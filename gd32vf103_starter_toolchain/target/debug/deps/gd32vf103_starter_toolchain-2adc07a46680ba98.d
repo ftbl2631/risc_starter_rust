28b1377525911a5b9852a96ced60cd2988c7567d
ftbl2631 <ftbl2631@qq.com>
2022-12-25 15:56:26 +1300

    initial initial toolchain part

---
+++ gd32vf103_starter_toolchain/target/debug/deps/gd32vf103_starter_toolchain-2adc07a46680ba98.d
@@ -0,0 +1,5 @@
+/home/fab/sharefile/projects/practice/rust_lang/risc_starter_rust/gd32vf103_starter_toolchain/target/debug/deps/gd32vf103_starter_toolchain-2adc07a46680ba98.rmeta: src/main.rs
+
+/home/fab/sharefile/projects/practice/rust_lang/risc_starter_rust/gd32vf103_starter_toolchain/target/debug/deps/gd32vf103_starter_toolchain-2adc07a46680ba98.d: src/main.rs
+
+src/main.rs:
---
+++ gd32vf103_starter_toolchain/target/debug/deps/gd32vf103_starter_toolchain-91a7bf486f800a9e.d
@@ -0,0 +1,5 @@
+/home/fab/sharefile/projects/practice/rust_lang/risc_starter_rust/gd32vf103_starter_toolchain/target/debug/deps/gd32vf103_starter_toolchain-91a7bf486f800a9e.rmeta: src/main.rs
+
+/home/fab/sharefile/projects/practice/rust_lang/risc_starter_rust/gd32vf103_starter_toolchain/target/debug/deps/gd32vf103_starter_toolchain-91a7bf486f800a9e.d: src/main.rs
+
+src/main.rs:
